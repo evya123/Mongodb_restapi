54e582f0f80db7e0d1ac8778158801e9010ff447
Evyatar Shtern <evya123@gmail.com>
2021-03-20 21:50:06 +0200

    added helpers

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const cors = require("cors");
 const { MONGO_URI } = require('./config');
 const mongoose = require("mongoose");
 const router = require("./routes/authority");
+const logger = require("morgan");
+const indexRouter = require("./routes/index");
+const apiResponse = require("./helpers/apiResponse");
 
+//
 mongoose.connect(MONGO_URI, { 
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
@@ -43,12 +47,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 //Route Prefixes
+app.use('/',indexRouter);
 app.use('/api/authorities', authorityRouter);
 
 app.get('/',async(_, res) => {
 	console.log('In /')
 	res.json({msg: "App is working!"})
 })
+
+// throw 404 if URL not found
+app.all("*", function(req, res) {
+	return apiResponse.notFoundResponse(res, "Page not found");
+});
 
 app.listen(3000, () => {
   console.log("Listening to port 3000");
